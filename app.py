e0b2527930dcca10657e132cd1e93144edeb8a66
Aaron Colvin <adcmyb@mst.edu>
2021-04-10 04:12:53 -0500

    Added button link to map

--- app.py
+++ app.py
@@ -8,6 +8,10 @@ app = Flask(__name__)
 def home():
     return render_template('main_index.html')
 
+@app.route('/pokemap.html')
+def pokemap():
+    return render_template('pokemap.html')
+
 if __name__ == "__main__":
     app.secret_key = os.urandom(24)
     app.run(debug=True,host='0.0.0.0', port=80)
