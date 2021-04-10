6fa109d432c9140d06defecabfbbdbe5e043b382
David Shelby <dshelby@ntst.com>
2021-04-10 11:06:20 -0500

    adding more firebase stuff

---
+++ static/js/google_auth.js
@@ -0,0 +1,175 @@
+// These samples are intended for Web so this import would normally be
+// done in HTML however using modules here is more convenient for
+// ensuring sample correctness offline.
+// import firebase from "firebase/app";
+// import "firebase/auth";
+
+// Docs: https://source.corp.google.com/piper///depot/google3/third_party/devsite/firebase/en/docs/auth/web/google-signin.md
+
+function googleProvider() {
+  // [START auth_google_provider_create]
+  var provider = new firebase.auth.GoogleAuthProvider();
+  // [END auth_google_provider_create]
+
+  // [START auth_google_provider_scopes]
+  provider.addScope('https://www.googleapis.com/auth/contacts.readonly');
+  // [END auth_google_provider_scopes]
+
+  // [START auth_google_provider_params]
+  provider.setCustomParameters({
+    'login_hint': 'user@example.com'
+  });
+  // [END auth_google_provider_params]
+}
+
+function googleSignInPopup() {
+  // [START auth_google_signin_popup]
+    const config = {
+    apiKey: "AIzaSyDp9mGuQY_bP3P8dTjbyKOrjke-hyo4eAg",
+    authDomain: "pickhacks2021.firebaseapp.com",
+    databaseURL: "https://pickhacks2021-default-rtdb.firebaseio.com",
+    projectId: "pickhacks2021",
+    storageBucket: "pickhacks2021.appspot.com",
+    messagingSenderId: "772315144443",
+    appId: "1:772315144443:web:4f3221964df004d27dd05e",
+    measurementId: "G-X45W2P7QR5"
+  };
+  firebase.initializeApp(config);
+  var provider = new firebase.auth.GoogleAuthProvider();
+  firebase.auth()
+    .signInWithPopup(provider)
+    .then((result) => {
+      /** @type {firebase.auth.OAuthCredential} */
+      var credential = result.credential;
+
+      // This gives you a Google Access Token. You can use it to access the Google API.
+      var token = credential.accessToken;
+      // The signed-in user info.
+      var user = result.user;
+      if (user != null) {
+        user.providerData.forEach(function (profile) {
+          firebase.database().ref().child("sessions/" + profile.uid).set({
+            "userId": profile.uid,
+            "email": profile.email
+          });
+        });
+      }
+      // ...
+    }).catch((error) => {
+      // Handle Errors here.
+      var errorCode = error.code;
+      var errorMessage = error.message;
+      // The email of the user's account used.
+      var email = error.email;
+      // The firebase.auth.AuthCredential type that was used.
+      var credential = error.credential;
+      // ...
+    });
+  // [END auth_google_signin_popup]
+}
+
+function googleSignInRedirectResult() {
+  // [START auth_google_signin_redirect_result]
+  firebase.auth()
+    .getRedirectResult()
+    .then((result) => {
+      if (result.credential) {
+        /** @type {firebase.auth.OAuthCredential} */
+        var credential = result.credential;
+
+        // This gives you a Google Access Token. You can use it to access the Google API.
+        var token = credential.accessToken;
+        // ...
+      }
+      // The signed-in user info.
+      var user = result.user;
+    }).catch((error) => {
+      // Handle Errors here.
+      var errorCode = error.code;
+      var errorMessage = error.message;
+      // The email of the user's account used.
+      var email = error.email;
+      // The firebase.auth.AuthCredential type that was used.
+      var credential = error.credential;
+      // ...
+    });
+  // [END auth_google_signin_redirect_result]
+}
+
+function googleBuildAndSignIn(id_token) {
+  // [START auth_google_build_signin]
+  // Build Firebase credential with the Google ID token.
+  var credential = firebase.auth.GoogleAuthProvider.credential(id_token);
+
+  // Sign in with credential from the Google user.
+  firebase.auth().signInWithCredential(credential).catch((error) => {
+    // Handle Errors here.
+    var errorCode = error.code;
+    var errorMessage = error.message;
+    // The email of the user's account used.
+    var email = error.email;
+    // The firebase.auth.AuthCredential type that was used.
+    var credential = error.credential;
+    // ...
+  });
+  // [END auth_google_build_signin]
+}
+
+function create_session() {
+  var user = firebase.auth().currentUser;
+  if (user != null) {
+    firebase.database().ref().child('sessions').set({
+      userId: profile.uid,
+      name: profile.displayName,
+    });
+  };
+
+}
+
+// [START auth_google_callback]
+function onSignIn(googleUser) {
+  console.log('Google Auth Response', googleUser);
+  // We need to register an Observer on Firebase Auth to make sure auth is initialized.
+  var unsubscribe = firebase.auth().onAuthStateChanged((firebaseUser) => {
+    unsubscribe();
+    // Check if we are already signed-in Firebase with the correct user.
+    if (!isUserEqual(googleUser, firebaseUser)) {
+      // Build Firebase credential with the Google ID token.
+      var credential = firebase.auth.GoogleAuthProvider.credential(
+          googleUser.getAuthResponse().id_token);
+
+      // Sign in with credential from the Google user.
+      // [START auth_google_signin_credential]
+      firebase.auth().signInWithCredential(credential).catch((error) => {
+        // Handle Errors here.
+        var errorCode = error.code;
+        var errorMessage = error.message;
+        // The email of the user's account used.
+        var email = error.email;
+        // The firebase.auth.AuthCredential type that was used.
+        var credential = error.credential;
+        // ...
+      });
+      // [END auth_google_signin_credential]
+    } else {
+      console.log('User already signed-in Firebase.');
+    }
+  });
+}
+// [END auth_google_callback]
+
+// [START auth_google_checksameuser]
+function isUserEqual(googleUser, firebaseUser) {
+  if (firebaseUser) {
+    var providerData = firebaseUser.providerData;
+    for (var i = 0; i < providerData.length; i++) {
+      if (providerData[i].providerId === firebase.auth.GoogleAuthProvider.PROVIDER_ID &&
+          providerData[i].uid === googleUser.getBasicProfile().getId()) {
+        // We don't need to reauth the Firebase connection.
+        return true;
+      }
+    }
+  }
+  return false;
+}
+// [END auth_google_checksameuser]
