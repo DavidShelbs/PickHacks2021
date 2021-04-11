48026aa74daec74df6d3705c42333fba0d12a36a
Andrew Sunarto <adsunarto@Andrews-MacBook-Pro.local>
2021-04-10 19:16:38 -0500

    Leaderboard and Statistics pages

--- app.py
+++ app.py
@@ -46,6 +46,14 @@ def pokemap():
 def game():
 	return render_template('game.html')
 
+@app.route('/leaderboards.html')
+def leaderboards():
+    return render_template('leaderboards.html')
+
+@app.route('/statistics.html')
+def statistics():
+    return render_template('statistics.html')
+
 if __name__ == "__main__":
     app.secret_key = os.urandom(24)
     app.run(debug=True,host='0.0.0.0', port=80)
